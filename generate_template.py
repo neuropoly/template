ec035d50c6ccca5738f943ffa9817e5ea6be0b21
Benjamin De Leener <jackbdl@gmail.com>
2017-09-21 07:19:55 -0400

    NEW: script for generating the template with the IPL pipeline

---
+++ generate_template.py
@@ -0,0 +1,23 @@
+"""
+Run this script with the following command (replace N by the number of subjects):
+
+python -m scoop -n N -vvv generate_template.py
+
+"""
+
+from scoop import futures, shared
+
+import iplScoopGenerateModel as gm
+
+if __name__ == '__main__':
+    # setup data for parallel processing
+    gm.generate_nonlinear_model_csv('subjects.csv',
+                                    work_prefix='model_nl_all',
+                                    options={'symmetric': True,
+                                             'protocol': [{'iter': 4, 'level': 8},
+                                                          {'iter': 4, 'level': 4},
+                                                          {'iter': 4, 'level': 2},
+                                                          {'iter': 4, 'level': 1}],
+                                             'refine': True
+                                             }
+                                    )
