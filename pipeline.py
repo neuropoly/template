9e954a3a17b8a5d900e9935d0ba67e88c20475fe
NadiaBlostein <nadia.blostein@mail.mcgill.ca>
2023-04-17 09:47:57 -0400

    updating pipeline according to preprocessing fxns

--- pipeline.py
+++ pipeline.py
@@ -4,38 +4,32 @@ The default dataset is an example dataset that is downloaded at the beginning of
 """
 
 from preprocessing import *
+import sys
 
-# downloading data and configuration file from OSF
-path_data = download_data_template(path_data='./')
-
-# extracting info from dataset
-dataset_info = read_dataset(path_data + 'configuration.json', path_data=path_data)
+dataset_info = read_dataset(sys.argv[1])
 
 # generating centerlines
-list_centerline = generate_centerline(dataset_info=dataset_info, contrast='t1', regenerate=True)
+list_centerline = generate_centerline(dataset_info = dataset_info, regenerate = True) 
 
 # computing average template centerline and vertebral distribution
-points_average_centerline, position_template_disks = average_centerline(list_centerline=list_centerline,
-                                                                        dataset_info=dataset_info,
-                                                                        use_ICBM152=False,
-                                                                        use_label_ref='C1')
+points_average_centerline, position_template_discs = average_centerline(list_centerline = list_centerline,
+	dataset_info = dataset_info,
+	use_ICBM152 = False,
+	use_label_ref = 'C1')
 
 # generating the initial template space
-generate_initial_template_space(dataset_info=dataset_info,
-                                points_average_centerline=points_average_centerline,
-                                position_template_disks=position_template_disks)
+generate_initial_template_space(dataset_info = dataset_info,
+	points_average_centerline = points_average_centerline,
+	position_template_discs = position_template_discs)
 
 # straightening of all spinal cord
-straighten_all_subjects(dataset_info=dataset_info, contrast='t1')
+straighten_all_subjects(dataset_info = dataset_info)
 
 # normalize image intensity inside the spinal cord
-normalize_intensity_template(dataset_info=dataset_info,
-                             fname_template_centerline=dataset_info['path_template'] + 'template_centerline.npz',
-                             contrast='t1',
-                             verbose=1)
+normalize_intensity_template(dataset_info = dataset_info)
 
 # copy preprocessed dataset in template folder
-copy_preprocessed_images(dataset_info=dataset_info, contrast='t1')
+copy_preprocessed_images(dataset_info = dataset_info)
 
 # converting results to Minc format
-convert_data2mnc(dataset_info, contrast='t1')
+convert_data2mnc(dataset_info)
