9b4d4c6fda6c4d68653fc6d6efe1416e17f64b3d
NadiaBlostein <nadia.blostein@mail.mcgill.ca>
2023-06-26 15:16:03 -0400

    making data path name generic

--- segment_batching_script.sh
+++ segment_batching_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sct_run_batch -jobs 6 -path-data "../philadelphia-pediatric" \
+sct_run_batch -jobs 6 -path-data "/PATH/TO/dataset" \
 	-subject-prefix "sub-" -exclude-list sub-107 sub-125 -script segment_sc_discs_deepseg.sh \
-	-path-output "../philadelphia-pediatric/derivatives/labels"
+	-path-output "/PATH/TO/dataset/derivatives/labels"
 
