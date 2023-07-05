12c51d9c338376cc3a070adbf29651737e045338
Nadia Blostein <p117938@abbey.neuro.polymtl.ca>
2023-07-05 18:15:46 -0400

    resolving issue #47: removing redundant lists  and

--- template_preprocessing.py
+++ template_preprocessing.py
@@ -16,20 +16,6 @@ from spinalcordtoolbox.download import download_data, unzip
 
 list_labels = [50, 49, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
                27, 28, 29, 30]
-labels_regions = {'PMJ': 50, 'PMG': 49,
-                  'C1': 1, 'C2': 2, 'C3': 3, 'C4': 4, 'C5': 5, 'C6': 6, 'C7': 7,
-                  'T1': 8, 'T2': 9, 'T3': 10, 'T4': 11, 'T5': 12, 'T6': 13, 'T7': 14, 'T8': 15, 'T9': 16, 'T10': 17,
-                  'T11': 18, 'T12': 19,
-                  'L1': 20, 'L2': 21, 'L3': 22, 'L4': 23, 'L5': 24,
-                  'S1': 25, 'S2': 26, 'S3': 27, 'S4': 28, 'S5': 29,
-                  'Co': 30}
-regions_labels = {'50': 'PMJ', '49': 'PMG',
-                  '1': 'C1', '2': 'C2', '3': 'C3', '4': 'C4', '5': 'C5', '6': 'C6', '7': 'C7',
-                  '8': 'T1', '9': 'T2', '10': 'T3', '11': 'T4', '12': 'T5', '13': 'T6', '14': 'T7', '15': 'T8',
-                  '16': 'T9', '17': 'T10', '18': 'T11', '19': 'T12',
-                  '20': 'L1', '21': 'L2', '22': 'L3', '23': 'L4', '24': 'L5',
-                  '25': 'S1', '26': 'S2', '27': 'S3', '28': 'S4', '29': 'S5',
-                  '30': 'Co'}
 
 def average_coordinates_over_slices(self, image): ### deprecated from latest version of spinalcordtoolbox
     # extracting points information for each coordinates
@@ -246,8 +232,8 @@ def average_centerline(list_centerline, dataset_info, use_ICBM152 = False, use_l
                 elif disc_label == 'PMG':
                     length = abs(dist_discs[disc_label] - dist_discs['C1'])
                 else:
-                    index_current_label = list_labels.index(labels_regions[disc_label])
-                    next_label = regions_labels[str(list_labels[index_current_label + 1])]
+                    index_current_label = list_labels.index(Centerline.labels_regions[disc_label])
+                    next_label = Centerline.regions_labels[list_labels[index_current_label + 1]]
                     if next_label in dist_discs:
                         length = abs(dist_discs[disc_label] - dist_discs[next_label])
                         if disc_label in new_vert_length:
@@ -267,8 +253,8 @@ def average_centerline(list_centerline, dataset_info, use_ICBM152 = False, use_l
                 elif disc_label == 'PMG':
                     length = abs(dist_discs[disc_label] - dist_discs['C1'])
                 else:
-                    index_current_label = list_labels.index(labels_regions[disc_label])
-                    next_label = regions_labels[str(list_labels[index_current_label + 1])]
+                    index_current_label = list_labels.index(Centerline.labels_regions[disc_label])
+                    next_label = Centerline.regions_labels[list_labels[index_current_label + 1]]
                     if next_label in dist_discs:
                         length = abs(dist_discs[disc_label] - dist_discs[next_label])
                     else:
@@ -294,8 +280,8 @@ def average_centerline(list_centerline, dataset_info, use_ICBM152 = False, use_l
         if 'PMJ' in average_length:
             distances_discs_from_C1['PMJ'] = -average_length['PMG'][1] - average_length['PMJ'][1]
     for disc_number in list_labels:
-        if disc_number not in [50, 49, 1] and regions_labels[str(disc_number)] in average_length:
-            distances_discs_from_C1[regions_labels[str(disc_number)]] = distances_discs_from_C1[regions_labels[str(disc_number - 1)]] + average_length[regions_labels[str(disc_number)]][1]
+        if disc_number not in [50, 49, 1] and Centerline.regions_labels[disc_number] in average_length:
+            distances_discs_from_C1[Centerline.regions_labels[disc_number]] = distances_discs_from_C1[Centerline.regions_labels[disc_number - 1]] + average_length[Centerline.regions_labels[disc_number]][1]
 
     # calculating discs average distances from C1
     average_distances = []
@@ -384,8 +370,8 @@ def average_centerline(list_centerline, dataset_info, use_ICBM152 = False, use_l
             temp_point = np.copy(coord_ref)
 
             if i >= index_straight:
-                index_current_label = list_labels.index(labels_regions[current_label])
-                next_label = regions_labels[str(list_labels[index_current_label + 1])]
+                index_current_label = list_labels.index(Centerline.labels_regions[current_label])
+                next_label = Centerline.regions_labels[list_labels[index_current_label + 1]]
                 if next_label not in average_positions_from_C1:
                     temp_point[2] = coord_ref[2] - average_positions_from_C1[current_label] - relative_position_from_disc * length_current_label
                 else:
@@ -451,7 +437,7 @@ def generate_initial_template_space(dataset_info, points_average_centerline, pos
     coord_physical = []
     image_discs = template_space.copy()
     for disc in position_template_discs:
-        label = labels_regions[disc]
+        label = Centerline.labels_regions[disc]
         coord = position_template_discs[disc]
         coord_pix = image_discs.transfo_phys2pix([coord])[0]
 
