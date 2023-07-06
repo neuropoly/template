e37563517e05196f5927de4612f38476891264ea
jcohenadad <jcohen@polymtl.ca>
2023-07-06 09:47:26 -0400

    Removed as per #39

--- preprocess_label.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sct_run_batch -jobs 6 -path-data "/PATH/TO/dataset" \
-        -subject-prefix "sub-" -exclude-list sub-107 sub-125 -script segment_subjects.sh \
-	-path-output "/PATH/TO/dataset/derivatives/labels"
-
