5a2e4e75a5b1ccd9e8216bb8e0f74a00b10ab05e
Nadia Blostein <p117938@abbey.neuro.polymtl.ca>
2023-07-06 00:19:02 -0400

    naming files more clearly and updating README

--- preprocess_label.sh
+++ preprocess_label.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sct_run_batch -jobs 6 -path-data "/PATH/TO/dataset" \
-	-subject-prefix "sub-" -exclude-list sub-107 sub-125 -script segment_sc_discs_deepseg.sh \
+        -subject-prefix "sub-" -exclude-list sub-107 sub-125 -script segment_subjects.sh \
 	-path-output "/PATH/TO/dataset/derivatives/labels"
 
